5de3cfc026f3cf4ca3a2efad640bab51c827b065
deb331474 <deb331474@gmail.com>
2023-09-02 23:53:05 +0530

    Added Add to cart page

---
+++ src/test/java/PageObject/AddToCartPage.java
@@ -0,0 +1,14 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class AddToCartPage extends BasePage{
+
+	public AddToCartPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
